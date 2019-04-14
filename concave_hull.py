b4cecabf9b571485c808447dd25f2d2891f4af31
amarschn <andrew.marschner@gmail.com>
2019-04-14 15:28:38 -0500

    Semi-working concave hull

--- concave_hull.py
+++ concave_hull.py
@@ -1,98 +1,147 @@
-"""
-
-
-Source: http://blog.thehumangeo.com/2014/05/12/drawing-boundaries-in-python/
-"""
-import shapely
-from shapely.ops import cascaded_union, polygonize
-import scipy.spatial
+from scipy.spatial import cKDTree
+import shapely.geometry
 import numpy as np
 import math
 import ipdb
 
 
-def concave_hull(points, alpha):
-    """
-    Compute the alpha shape (concave hull) of a set
-    of points.
-    @param points: list of (x,y) tuples
-    @param alpha: alpha value to influence the
-        gooeyness of the border. Smaller numbers
-        don't fall inward as much as larger numbers.
-        Too large, and you lose everything!
-    """
-    points = tuple(map(tuple, points))
-    if len(points) < 4:
-        # When you have a triangle, there is no sense
-        # in computing an alpha shape.
-        return shapely.geometry.MultiPoint(points).convex_hull
+def concave_hull(points, k):
+	"""
+	Paper: "CONCAVE HULL: A K-NEAREST NEIGHBOURS APPROACH FOR THE COMPUTATION OF THE REGION OCCUPIED BY A SET OF POINTS"
+	Implementation: https://github.com/joaofig/uk-accidents/blob/master/geomath/hulls.py
+	https://pdfs.semanticscholar.org/2397/17005c3ebd5d6a42fc833daf97a0edee1ce4.pdf
+	points: 2D array of XY points in the format [(x1,y1), (x2,y2), ...]
+	k: number of NN to use in algorithm
+	"""
+	
+	kk = max(k, 3)
+	points = set(map(tuple, points))
+	if len(points) < 3:
+		return []
+	if len(points) == 3:
+		return points
+	kk = min(kk, len(points) - 1)
 
-    def add_edge(edges, edge_points, p1, p2):
-        """
-        Add a line between the i-th and j-th points,
-        if not in the list already
-        """
-        if (p1, p2) in edges or (p2, p1) in edges:
-            # already added
-            return
-        edges.add((p1, p2))
-        edge_points.append((points[p1], points[p2]))
-        # edge_points.append(points[p1])
-        # edge_points.append(points[p2])
+	# Construct a KD tree for use in knn search
+	tree = cKDTree(np.array(list(points)), copy_data=True, balanced_tree=True)
 
-    tri = scipy.spatial.Delaunay(points)
-    edges = set()
-    edge_points = []
-    # loop over triangles:
-    # ia, ib, ic = indices of corner points of the
-    # triangle
-    for ia, ib, ic in tri.vertices:
-    	# ipdb.set_trace()
-        pa = points[ia]
-        pb = points[ib]
-        pc = points[ic]
+	# Set first point to the minimum y-value (why y-value?)
+	# ipdb.set_trace()
+	first = min(points, key=lambda pt: pt[1])
+	hull = np.array([first])
+	current = first
+	points.remove(first)
+	# Set previous to a point slightly to the right of the current point (angle of 0deg)
+	previous = (current[0] + 1, current[1])
+	step = 2
 
-        # Lengths of sides of triangle
-        len_a = math.sqrt((pa[0]-pb[0])**2 + (pa[1]-pb[1])**2)
-        len_b = math.sqrt((pb[0]-pc[0])**2 + (pb[1]-pc[1])**2)
-        len_c = math.sqrt((pc[0]-pa[0])**2 + (pc[1]-pa[1])**2)
+	while ((current != first or step == 2) and (len(points) > 0)):
+		# At step 5 it is possible to create a >3-sided polygon
+		if step == 5:
+			points.add(first)
+		# ipdb.set_trace()
+		nearest_pts = knn(tree, k, current, previous)
+		candidates = rank_points(nearest_pts, current, previous)
+		# ipdb.set_trace()
+		i = 0
+		invalid_hull = True
 
-        # Semiperimeter of triangle
-        s = (len_a + len_b + len_c)/2.0
+		while invalid_hull and i < len(candidates):
+			candidate = candidates[i]
+			c = tuple(candidate)
 
-        # Area of triangle by Heron's formula
-        area = math.sqrt(s*(s-len_a)*(s-len_b)*(s-len_c))
-        circum_r = len_a*len_b*len_c/(4.0*area)
+			# Create a test hull to check if there is any self interesection
+			test_hull = hull[:]
+			# ipdb.set_trace()
+			test_hull = np.append(test_hull, [candidate], axis=0)
+			line = shapely.geometry.asLineString(test_hull)
+			invalid_hull = not line.is_simple
+			i += 1
 
-        # Here's the radius filter.
-        #print circum_r
-        if circum_r < 1.0/alpha:
-            add_edge(edges, edge_points, ia, ib)
-            add_edge(edges, edge_points, ib, ic)
-            add_edge(edges, edge_points, ic, ia)
-    m = shapely.geometry.MultiLineString(edge_points)
+		# If all points still intersect, it means we need to get new points to examine
+		if invalid_hull:
+			# ipdb.set_trace()
+			print candidates
+			print current
+			print "invalid hull"
+			return hull
+		previous = current
+		current = tuple(candidate)
+		# ipdb.set_trace()
+		points.remove(current)
+		hull = test_hull
+		# print hull
+		step += 1
+	return hull
 
-    triangles = list(polygonize(m))
 
-    return cascaded_union(triangles), edge_points
-    # return edge_points
+def knn(kdtree, k, current, previous):
+	"""
+	Gets k nearest neighbors from kdtree, removing currenet and previous points
+	if necessary
+	"""
+	_, nearest_pts_idx = kdtree.query(current, k=k)
+	nearest_pts = kdtree.data[nearest_pts_idx]
+	filtered_pts = []
+	# ipdb.set_trace()
+	for pt in nearest_pts:
+		if not all(pt == current) and not all(pt == previous):
+			filtered_pts.append(pt)
+
+	return np.array(filtered_pts)
+
+
+
+
+def rank_points(pts, current, previous):
+	"""
+	Ranks points according to 
+	"""
+	angles = np.array([])
+	ordered_points = np.array([])
+	# ipdb.set_trace()
+	v1 = (current[0] - previous[0], current[1] - previous[1])
+	for pt in pts:
+		v2 = (pt[0] - current[0], pt[1] - current[1])
+		angles = np.append(angles, get_angle(v1, v2))
+	# ipdb.set_trace()
+	return pts[np.argsort(angles)[::-1]]
+
+
+def get_angle(v1, v2):
+	"""
+	Returns angle between 2 vectors
+	"""
+	(x1, y1) = v1
+	(x2, y2) = v2
+	return math.atan2(x1*y2 - y1*x2, x1*x2 + y1*y2)
+
+
+
 
 if __name__ == '__main__':
+	# pts = [(-1,0.1), (0,1), (1,0)]
+	# current = (0,0)
+	# previous = (-1,0)
+	# print rank_points(pts, current, previous)
 	import matplotlib.pyplot as plt
 	POINTS = np.loadtxt('h_pts.csv')
-	hull, _ = concave_hull(POINTS, alpha=0.06)
-	points = []
-	try:
-		for poly in hull:
-			points.extend(list(poly.exterior.coords))
-	except:
-		points = list(hull.exterior.coords)
+	# POINTS = np.loadtxt('test_pts.csv')
+	# hull, _ = concave_hull(POINTS, alpha=0.06)
+	print(len(POINTS))
+	hull = concave_hull(POINTS, k=5)
+	# points = []
+	# try:
+	# 	for poly in hull:
+	# 		points.extend(list(poly.exterior.coords))
+	# except:
+	# 	points = list(hull.exterior.coords)
 	# ipdb.set_trace()
 	# points = list(hull[0].exterior.coords)
 	# for poly in 
 
 	# edge_points = concave_hull(POINTS, alpha=0.5)
-	
-	plt.plot(*zip(*points), marker='o', linestyle=None)
+	# plt.plot(*zip(*POINTS), color='r', marker='x', linestyle='None')
+	plt.plot(*zip(*hull), marker='o', linestyle='None')
 	plt.show()
 

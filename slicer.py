caa57bf0f35a834ee1d8a8812b12016a7a18eb4d
amarschn <andrew.marschner@gmail.com>
2019-04-16 14:38:01 -0500

    adds files, modifies ordering code

--- slicer.py
+++ slicer.py
@@ -5,37 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import math
 import pprint
 from collections import deque
-import ipdb
-
-
-def get_intersect(segment, layer):
-    """
-    Returns tuple intersect point of a line segment and a given layer height
-    """
-    # If the segment top or bottom intersects the layer, return that point
-    if layer > segment[0][2] and layer > segment[1][2]:
-        return ()
-    elif layer < segment[0][2] and layer < segment[1][2]:
-        return ()
-    elif segment[0][2] == layer:
-        return tuple(segment[0])
-    elif segment[1][2] == layer:
-        return tuple(segment[1])
-    else:
-        dx = segment[1][0] - segment[0][0]
-        dy = segment[1][1] - segment[0][1]
-        dz = segment[1][2] - segment[0][2]
-        p = abs((layer - min(segment[0][2], segment[1][2]))/dz)
-
-        # Account for segments that point "down" or "up" in Z
-        if dz > 0:
-            ix = segment[0][0] + dx*p
-            iy = segment[0][1] + dy*p
-        else:
-            ix = segment[1][0] - dx*p
-            iy = segment[1][1] - dy*p
-
-        return (ix, iy, layer)
+# import ipdb
 
 
 def plot_all_segments(segments):
@@ -76,91 +46,45 @@ def plot_individual_segments(segments):
     plt.show()
 
 
-def brute_order_segments(segments):
+def make_polygons(s, tol = .005):
     """
     Takes in all the segments of a layer, and orders them such that every
     segment is connected at both ends to another segment, and no segment
-    end is connected to more than 1 other segment end
+    end is connected to more than 1 other segment end.
 
-    TODO: is this even necessary for a bitmap-based output?
+    This will can create multiple polygons
+
+    TODO: is this even necessary for a bitmap-based output? Answer - Pretty much yes.
+    TODO: implement Bentley Ottmann algorithm to take this from O(n2) to O(n log n)
     """
+    segments = s.copy()
+    print(len(segments))
+    polygons = []
     D = deque([segments.pop()])
-    # ipdb.set_trace()
     while segments:
         # Create beginning and end of chain
         start = D[0][0]
         end = D[-1][1]
+        print(D, len(D))
         for seg in segments:
             seg_start = seg[0]
             seg_end = seg[1]
             # if the first point of the first segment is equal to the second
             # point of the second segment, then add the second segment to the
             # top of the deque
-            if np.isclose(start, seg_start).all():
-                segments.remove(seg)
-                seg = [(seg_end, seg_start)]
-                D.appendleft(seg)
-            elif np.isclose(start, seg_end).all():
+            if np.isclose(start, seg_end, tol).all():
                 segments.remove(seg)
                 D.appendleft(seg)
-            elif np.isclose(end, seg_end).all():
-                segments.remove(seg)
-                seg = [(seg_end, seg_start)]
-                D.append(seg)
-            elif np.isclose(end, seg_start).all():
+                start = seg_start
+            elif np.isclose(end, seg_start, tol).all():
                 segments.remove(seg)
                 D.append(seg)
-    return D
-
-
-def layers(mesh, resolution):
-    height = mesh.z.max() - mesh.z.min()
-    slices = np.array([z for z in range(int(height/resolution) + 1)])*resolution
-    layers = []
-    for layer in slices:
-        segments = get_layer_segments(mesh, layer)
-        if segments:
-            layers.append(segments)
-    return layers
-
-def get_layer_segments(mesh, layer):
-    # For each slice, loop through all triangles and determine if they intersect
-    # with that slice
-    segments = []
-    for triangle in mesh:
-        p1 = triangle[0:3]
-        p2 = triangle[3:6]
-        p3 = triangle[6:]
-        z = np.array([triangle[2], triangle[5], triangle[8]])
-        # If the layer is too high or too low for the triangle, don't worry
-        # about slicing, this triangle is out of the relevant range
-        if layer >= max(z) or layer <= min(z):
-            continue
-        # If the triangle has points both above and below the z, create
-        # segments from the intersection of the triangle and the layer
-
-        # Some rules about intersections:
-        # There can only be 2 intersections of a triangle for a valid slice, no more and no less
-        # Single intersections correspond to a tangent or point meeting of the triangle to a slice plane
-        # Infinite intersections correspond to a situation where the triangle or triangle segment is parallel to the slice plane
-
-        # For each segment, find if there is an intersection with the slice plane
-        # Calculate the intersection via interpolation
-        seg1 = np.array([p1, p2])
-        seg2 = np.array([p2, p3])
-        seg3 = np.array([p3, p1])
-        segs = [seg1, seg2, seg3]
-        # print segs
-        # print "\n\n"
-        intersects = []
-
-        for seg in segs:
-            intersect = get_intersect(seg, layer)
-            if intersect:
-                intersects.append(intersect)
-        if len(intersects) == 2:
-            segments.append(intersects)
-    return segments
+                end = seg_end
+        if len(D) > 1 and np.isclose(start, end, tol).all():
+            polygons.append(D)
+            if segments:
+                D = deque([segments.pop()])
+    return polygons
 
 
 def get_segments(mesh, resolution):
@@ -226,8 +150,8 @@ def get_segments(mesh, resolution):
                 # Not all cases create a segment
                 continue
             if segment:
-                if segment[1][0] == 6:
-                    ipdb.set_trace()
+                # if segment[1][0] == 6:
+                    # ipdb.set_trace()
                 slices[z].append(segment)
     return slices
 
@@ -251,19 +175,24 @@ def interpolate(y, y0, y1, x0, x1):
     dx = x1 - x0
     dy = y1 - y0
 
-    p = y/dy
-
-    if p < 0:
-        x = dx * (-p) + x0
+    # If the slope is negative
+    if dy < 0:
+        # the proportion of the curve we are interpolating when the slope is negative is flipped
+        p = (y - max(y0, y1)) / dy
+        x = dx * p + x0
     else:
+        p = (y - min(y0, y1)) / dy
         x = dx * p + x0
     return x
 
 
 def main():
-    # f = './test_stl/cylinder.stl'
-    f = '../OpenGL-STL-slicer/prism.stl'
-    # f = '../OpenGL-STL-slicer/nist.stl'
+    # f = './test_stl/q01.stl'
+    f = './test_stl/cylinder.stl'
+    # f = './test_stl/prism.stl'
+    # f = './test_stl/nist.stl'
+    # f = './test_stl/hollow_prism.stl'
+    # f = './test_stl/10_side_hollow_prism.stl'
     mesh = stl.Mesh.from_file(f)
     resolution = 10.
     segments = get_segments(mesh, resolution)
@@ -272,16 +201,11 @@ def main():
     # print segments.shape
     # ipdb.set_trace()
     # print segments[10]
-    plot_individual_segments(segments[10])
-    # sliced_layers = layers(mesh, resolution)
-    # for layer in sliced_layers:
-    #     plot_individual_segments(layer)
-    # print len(sliced_layers)
-    # print("Unordered segments: ", segments)
-    # for s in segments:
-    #     print s
-    # print("Ordered segments: ", brute_order_segments(segments))
-    # plot_individual_segments(np.array(segments))
+    ordered = make_polygons(segments[10])
+    # print(ordered)
+    # plot_individual_segments(segments[10])
+    plot_individual_segments(ordered[1])
+
 
 
 if __name__ == '__main__':

cdc6ebd3725620be9a0ad4142c43f97b0e783509
amarschn <andrew.marschner@gmail.com>
2019-04-26 16:12:54 -0500

    Xor rasterizer

--- image_writer.py
+++ image_writer.py
@@ -3,6 +3,7 @@ from collections import deque
 import cairosvg
 import cv2
 
+
 def polygon_orientation(polygon):
     """
     TODO: this should be implemented in the initial polygon construction,
@@ -11,7 +12,7 @@ def polygon_orientation(polygon):
     whichever minimum (if there is a tie) has the maximum x, this will
     guarantee that the point lies on the convex hull of the polygon. Once
     this minimum point is found calculate the sign of the orientation matrix
-    between the three segments that the point is a part of
+    between the two segments that the point is a part of
 
     https://en.wikipedia.org/wiki/Curve_orientation
 
@@ -19,14 +20,13 @@ def polygon_orientation(polygon):
     -1 indicates counter clockwise
     1 indicates clockwise
     """
-    min_y = polygon[0][0][1]
-    min_y_x = polygon[0][0][0]
+    min_y = polygon[0][1]
+    min_y_x = polygon[0][0]
 
-    points = np.array(polygon)[:,0,:]
-    left_neighbor = points[-1]
-    right_neighbor = points[1]
+    left_neighbor = polygon[-1]
+    right_neighbor = polygon[1]
 
-    for idx, pt in enumerate(points):
+    for idx, pt in enumerate(polygon):
         x = pt[0]
         y = pt[1]
 
@@ -34,13 +34,14 @@ def polygon_orientation(polygon):
             min_y = y
             min_y_x = x
             if idx > 0:
-                left_neighbor = points[idx - 1]
+                left_neighbor = polygon[idx - 1]
             else:
-                left_neighbor = points[len(points)]
-            if idx < len(points) - 1:
-                right_neighbor = points[idx + 1]
+                left_neighbor = polygon[len(polygon) - 1]
+            if idx < len(polygon) - 1:
+                right_neighbor = polygon[idx + 1]
             else:
-                right_neighbor = points[0]
+                right_neighbor = polygon[0]
+
     xa = right_neighbor[0]
     ya = right_neighbor[1]
     xb = min_y_x
@@ -50,7 +51,10 @@ def polygon_orientation(polygon):
     det = (xb - xa)*(yc - ya) - (xc - xa)*(yb - ya)
     direction = np.sign(det)
     if direction == 0:
-        raise Exception("Unknown Polygon Direction")
+        # print("Polygon: {}".format(polygon))
+        # print("Minimum point: {}, {}".format(min_y_x, min_y))
+        # raise Exception("Unknown Polygon Direction")
+        print("Unknown Polygon Direction")
     return direction
 
 
@@ -114,38 +118,31 @@ def arrange_polygons(polygons):
         orientation = polygon_orientation(polygon)
         if orientation == -1:
             printed_polygons.append(polygon)
-        else:
+        elif orientation == 1:
             holes.append(polygon)
+        else:
+            continue
     return printed_polygons, holes
 
+def cv2_xor_rasterize(polygons, output_file, layer, height, width):
+    # printed_polygons, holes = arrange_polygons(polygons)
 
-def cv2_rasterize(polygons, height=4800, width=7200, layer=-1, output="cv2_raster.bmp"):
-    """
-
-    :param polygons:
-    :param height:
-    :param width:
-    :param output:
-    :return:
-    """
-    # for _ in range(64):
-
-    printed_polygons, holes = arrange_polygons(polygons)
-
-    printed_img = np.zeros([height, width])
-    printed_img.fill(255)
+    img = np.zeros([height, width])
+    holes = np.zeros([height, width])
 
     for polygon in polygons:
-        color = 0
-        points = np.array(polygon)[:, 0, :] * 24.606
-        points = points.reshape((-1, 1, 2))
-        cv2.fillPoly(printed_img, np.int32([points]), color)
+        orientation = polygon_orientation(polygon)
+        if orientation == -1:
+            points = np.array(polygon) * 24.606
+            cv2.fillPoly(img, np.int32([points]), 1)
+        elif orientation == 1:
+            points = np.array(polygon) * 24.606
+            cv2.fillPoly(holes, np.int32([points]), 1)
+        else:
+            continue
 
-    for hole in holes:
-        points = np.array(hole)[:, 0, :] * 24.606
-        points = points.reshape((-1, 1, 2))
-        color = 255
-        cv2.fillPoly(printed_img, np.int32([points]), color)
+    printed_img = np.logical_xor(img, holes)
+    printed_img = np.flip(printed_img, 0)
 
     cv2.putText(img=printed_img,
                 text=str(layer),
@@ -157,38 +154,38 @@ def cv2_rasterize(polygons, height=4800, width=7200, layer=-1, output="cv2_raste
                 lineType=1,
                 bottomLeftOrigin=False)
 
-    cv2.imwrite(output, printed_img)
+    cv2.imwrite(output_file, printed_img)
 
-def cv2_rasterize2(P):
+
+
+def cv2_rasterize(polygons, output_file, layer, height, width):
     """
 
-    :param polygons:
+    :param polygons: arrays of arrays of tuple points [(x1, y1), (x2, y2), ... ]
     :param height:
     :param width:
     :param output:
     :return:
     """
-    # for _ in range(64):
+    printed_polygons, holes = arrange_polygons(polygons)
 
-    printed_polygons, holes = arrange_polygons(P.polygons)
-
-    printed_img = np.zeros([P.height, P.width])
+    printed_img = np.zeros([height, width])
     printed_img.fill(255)
 
     for polygon in printed_polygons:
         color = 0
-        points = np.array(polygon)[:, 0, :] * 24.606
-        points = points.reshape((-1, 1, 2))
+        points = np.array(polygon) * 24.606
         cv2.fillPoly(printed_img, np.int32([points]), color)
 
     for hole in holes:
-        points = np.array(hole)[:, 0, :] * 24.606
-        points = points.reshape((-1, 1, 2))
+        points = np.array(hole) * 24.606
         color = 255
         cv2.fillPoly(printed_img, np.int32([points]), color)
 
+    printed_img = np.flip(printed_img, 0)
+
     cv2.putText(img=printed_img,
-                text=P.layer,
+                text=str(layer),
                 org=(250, 100),
                 fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                 fontScale=3,
@@ -197,11 +194,7 @@ def cv2_rasterize2(P):
                 lineType=1,
                 bottomLeftOrigin=False)
 
-    cv2.imwrite(P.output_file, printed_img)
-
-
-def f(name):
-    print('hello', name)
+    cv2.imwrite(output_file, printed_img)
 
 
 class Polygon(object):
@@ -250,6 +243,6 @@ if __name__ == '__main__':
     # 6s for bmp, 6.5s for png
     from multiprocessing import Pool
 
-    p = Pool(5)
-    p.map(cv2_rasterize2, Polygons)
+    # p = Pool(5)
+    # p.map(cv2_rasterize2, Polygons)
     ################################################################
--- slicer.py
+++ slicer.py
@@ -1,28 +1,10 @@
 import stl
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import math
-import pprint
-from collections import deque
-# import ipdb
 import networkx as nx
-
-def plot_all_segments(segments):
-    """
-    Uses matplotlib to plot all line segments in slice
-    Assumes that segments are an np array of shape (N, 3, 2)
-    """
-    x = segments[:, :, 0].flatten()
-    y = segments[:, :, 1].flatten()
-    z = segments[:, :, 2].flatten()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.plot(x, y, '.-')
-
-    plt.show()
+from image_writer import cv2_rasterize, cv2_xor_rasterize
+import os
+from multiprocessing import Pool
 
 
 def plot_individual_segments(segments):
@@ -45,27 +27,6 @@ def plot_individual_segments(segments):
         ax.arrow(x0, y0, x1 - x0, y1 - y0, head_width=0.5, length_includes_head=True)
     plt.show()
 
-
-def plot_polygons(polygons):
-    """
-    """
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    for poly in polygons:
-        for seg in poly:
-            # ipdb.set_trace()
-            x = [seg[0][0], seg[1][0]]
-            y = [seg[0][1], seg[1][1]]
-            ax.plot(x, y, '.', lineStyle='None')
-
-            x0 = seg[0][0]
-            x1 = seg[1][0]
-            y0 = seg[0][1]
-            y1 = seg[1][1]
-            ax.arrow(x0, y0, x1 - x0, y1 - y0, head_width=0.05, length_includes_head=True)
-    plt.show()
 
 
 def layer_graph(segments, decimal_place=3):
@@ -96,174 +57,7 @@ def layer_graph(segments, decimal_place=3):
 
     C = nx.simple_cycles(D)
 
-    return C
-
-def make_polygons(segments, decimal_place=3):
-    pt_dict = {}
-
-
-    for seg in np.round(segments, decimals=decimal_place):
-        p1 = tuple(seg[0])
-        p2 = tuple(seg[1])
-
-        if p1 == p2:
-            print("Segment is a point: {} : {}".format(p1, p2))
-            continue
-
-        if p1 in pt_dict:
-            if pt_dict[p1][1] != None:
-                print("P1 point already in dict: {}".format(p1))
-                print(p1, p2)
-                pt_dict[p1][1] = [pt_dict[p1][1], p2]
-                if pt_dict[p1][0] ==  pt_dict[p1][1]:
-                    print("Loop detected at point: {}".format(p1))
-            else:
-                pt_dict[p1][1] = p2
-        else:
-            pt_dict[p1] = [None, p2]
-
-        if p2 in pt_dict:
-            if pt_dict[p2][0] != None:
-                print("P2 point already in dict: {}".format(p2))
-                print(p1, p2)
-                pt_dict[p2][0] = [pt_dict[p2][0], p1]
-                # ipdb.set_trace()
-                if pt_dict[p2][0] == pt_dict[p2][1]:
-                    print("Loop detected at point: {}".format(p2))
-            else:
-                pt_dict[p2][0] = p1
-        else:
-            pt_dict[p2] = [p1, None]
-
-
-    # What to do about these things:
-    # Open segments
-    # Looped segments
-    # Somehow we must find any open segments and recursively remove their branches from the points dict
-    new_dict = pt_dict.copy()
-    for pt in pt_dict:
-        # right_neighbor = pt_dict[pt][1]
-
-        # if either the right neighbor has multiple values, it means that at this point there is a fork in the polygon
-        # This means that the branches starting at this point must be explored further to determine which 
-        # ends in a dead end
-        remove_branches(new_dict, pt)
-
-    pt_dict = new_dict
-    polygons = []
-    while len(pt_dict) > 0:
-        polygon = []
-        first_pt, [_, next_pt] = pt_dict.popitem()
-        polygon.append([first_pt, next_pt])
-
-        while first_pt != next_pt:
-            # ipdb.set_trace()
-            current_pt = next_pt
-            try:
-                _, next_pt = pt_dict.pop(current_pt)
-            except:
-                print("Failed to pop {}".format(current_pt))
-                plot_individual_segments(segments)
-                return
-            polygon.append([current_pt, next_pt])
-        polygons.append(polygon)
-
-    return polygons
-
-def remove_branches(pt_dict, pt):
-
-    if pt_dict.get(pt):
-        if type(pt) == list:
-            for i in pt:
-                remove_branches(pt_dict, pt[i])
-        else:
-            left_node = pt_dict[pt][0]
-            right_node = pt_dict[pt][1]
-
-            
-            if type(left_node) == list:
-                for n in left_node:
-                    left, _ = remove_branches(pt_dict, n)
-                    pt_dict[pt][0] = left
-            if type(right_node) == list:
-                for n in right_node:
-                    _, right = remove_branches(pt_dict, n)
-                    pt_dict[pt][1] = right
-            if left_node is None or right_node is None or left_node == right_node:
-                pt_dict.pop(pt)
-
-    return left_node, right_node
-
-    
-
-    # if type(right_neighbors) == list:
-    #     final_neighbor = right_neighbors[:]
-    #     for neighbor in right_neighbors:
-    #         if pt_dict[neighbor][0] == pt_dict[neighbor][1]:
-    #             final_neighbor.remove(neighbor)
-    #             print "Loop!"
-    #             print final_neighbor
-    #             new_pt_dict[pt][1] = final_neighbor[0]
-    #             if new_pt_dict.get(neighbor):
-    #                 new_pt_dict.pop(neighbor)
-
-    # if type(left_neighbors) == list:
-    #     final_neighbor = left_neighbors[:]
-    #     for neighbor in left_neighbors:
-    #         if pt_dict[neighbor][0] == pt_dict[neighbor][1]:
-    #             final_neighbor.remove(neighbor)
-    #             print "Loop!"
-    #             print final_neighbor
-    #             new_pt_dict[pt][0] = final_neighbor[0]
-    #             if new_pt_dict.get(neighbor):
-    #                 new_pt_dict.pop(neighbor)
-    
-    # return new_pt_dict
-
-def naive_make_polygons(s, tol = .005):
-    """
-    Takes in all the segments of a layer, and orders them such that every
-    segment is connected at both ends to another segment, and no segment
-    end is connected to more than 1 other segment end.
-
-    This will can create multiple polygons
-
-    TODO: is this even necessary for a bitmap-based output? Answer - Pretty much yes.
-    TODO: implement Bentley Ottmann algorithm to take this from O(n2) to O(n log n)
-    """
-    unaltered_segs = set(map(tuple, s))
-    segments = unaltered_segs.copy()
-    # print("Segment count: ", len(segments))
-    polygons = []
-    D = deque([segments.pop()])
-    while segments:
-        # Create beginning and end of chain
-        start = D[0][0]
-        end = D[-1][1]
-        # print(D, len(D))
-        for seg in unaltered_segs:
-            if seg not in segments:
-                continue
-
-            seg_start = seg[0]
-            seg_end = seg[1]
-            # if the first point of the first segment is equal to the second
-            # point of the second segment, then add the second segment to the
-            # top of the deque
-            if np.isclose(start, seg_end, atol = tol).all():
-                segments.remove(seg)
-                D.appendleft(seg)
-                start = seg_start
-            elif np.isclose(end, seg_start, atol = tol).all():
-                segments.remove(seg)
-                D.append(seg)
-                end = seg_end
-        if len(D) > 1 and np.isclose(start, end, atol = tol).all():
-            print("Ordered segment count: ", len(D))
-            polygons.append(D)
-            if segments:
-                D = deque([segments.pop()])
-    return polygons
+    return list(C)
 
 
 def get_unordered_slices(mesh, resolution):
@@ -281,8 +75,11 @@ def get_unordered_slices(mesh, resolution):
     layers = np.array([z for z in range(int(height/resolution) + 1)])*resolution
 
     slices = []
-    for layer in enumerate(layers):
-        slices.append([])
+    # slice_list = []
+    for i, layer in enumerate(layers):
+        sl = Slice(layer_number=i)
+        slices.append(sl)
+        # slice_list.append([])
 
     for triangle in mesh:
         p0 = triangle[0:3]
@@ -290,9 +87,6 @@ def get_unordered_slices(mesh, resolution):
         p2 = triangle[6:9]
 
         (z0, z1, z2) = p0[2], p1[2], p2[2]
-        # (y0, y1, y2) = round(p0[1]), round(p1[1]), round(p2[1])
-        # if y0 == y1 == y2 == 0:
-        #     print triangle
 
         for layer_num, z in enumerate(layers):
             segment = []
@@ -303,34 +97,34 @@ def get_unordered_slices(mesh, resolution):
             elif z0 < z and z1 >= z and z2 >= z:
                 # What condition is this?
                 segment = calculate_segment(p0, p2, p1, z)
-                # slices[z].append(segment)
+
             elif z0 > z and z1 < z and z2 < z:
                 # What condition is this?
                 segment = calculate_segment(p0, p1, p2, z)
-                # slices[z].append(segment)
+
             elif z0 >= z and z1 < z and z2 >= z:
                 # What condition is this?
                 segment = calculate_segment(p1, p0, p2, z)
-                # slices[z].append(segment)
+
             elif z0 < z and z1 > z and z2 < z:
                 # What condition is this?
                 segment = calculate_segment(p1, p2, p0, z)
-                # slices[z].append(segment)
+
             elif z0 >= z and z1 >= z and z2 < z:
                 # What condition is this?
                 segment = calculate_segment(p2, p1, p0, z)
-                # slices[z].append(segment)
+
             elif z0 < z and z1 < z and z2 > z:
                 # What condition is this?
                 segment = calculate_segment(p2, p0, p1, z)
-                # slices[z].append(segment)
+
             else:
                 # Not all cases create a segment
                 continue
+
             if segment:
-                # if segment[1][0] == 6:
-                    # ipdb.set_trace()
-                slices[layer_num].append(segment)
+                slices[layer_num].add_segment(segment)
+                # slice_list[layer_num].append(segment)
     return slices
 
 
@@ -385,7 +179,7 @@ def segment_test():
 
 def plot_polygon_points(polygons):
     """
-    Expects a generator of [(x1,y1), (x2,y2), ... ] arrays and will plot those array as a digraph of points to points
+    Expects an array of [(x1,y1), (x2,y2), ... ] arrays and will plot those array as a digraph of points to points
     :param polygon_points:
     :return:
     """
@@ -410,44 +204,65 @@ def plot_polygon_points(polygons):
     plt.show()
 
 
+def write_layer(S):
+    unordered_segments = S.segments
+    polygons = layer_graph(unordered_segments)
+    print(S.layer_number)
+    # plot_polygon_points(polygons)
+    cv2_xor_rasterize(polygons=polygons,
+                  output_file=S.filename,
+                  layer=S.layer_number,
+                  height=S.height,
+                  width=S.width)
+
+
+class Slice(object):
+    def __init__(self, segments=None, layer_number=-1, height=4800, width=7200):
+        self.filename = os.path.join('./output', "layer_{}.png".format(layer_number))
+        if segments is None:
+            self.segments = []
+        else:
+            self.segments = segments
+        self.layer_number = layer_number
+        self.height = 4800
+        self.width = 7200
+
+    def add_segment(self, segment):
+        self.segments.append(segment)
+
 def main():
     # f = './test_stl/logo.stl'
-    f = './test_stl/q01.stl'
+    # f = './test_stl/q01.stl'
     # f = './test_stl/cylinder.stl'
     # f = './test_stl/prism.stl'
     # f = './test_stl/nist.stl'
     # f = './test_stl/hollow_prism.stl'
     # f = './test_stl/10_side_hollow_prism.stl'
+    f = './test_stl/concentric_1.stl'
     mesh = stl.Mesh.from_file(f)
-    resolution = 0.05
-    slices = get_unordered_slices(mesh, resolution)
-    for i, s in enumerate(slices):
-        print(i)
-        polygons = layer_graph(s)
+    resolution = 1.0
+    Slices = get_unordered_slices(mesh, resolution)
+    for Slice in Slices:
+        write_layer(Slice)
+    # p = Pool(5)
+    # p.map(write_layer, Slices)
+
+
+
+    # for i, s in enumerate(slices):
+    #     print(i)
+    #     polygons = layer_graph(s)
+    #     vector_layers[i] = polygons
         # plot_polygon_points(polygons)
-    # ipdb.set_trace()
-    # print segments
-    # print segments.shape
-    # ipdb.set_trace()
-    # print segments[10]
-    # polygons = make_polygons(slices[2])
-    # print(polygons)
-    # plot_individual_segments(slices[2])
-    # plot_polygons(polygons)
 
-    # import pickle
+    # with open('vector_layers.json', 'w') as f:
+    #     json.dump(vector_layers, f)
 
-    # with open('q01', 'wb') as fp:
-    #     pickle.dump(polygons, fp)
-
-    # segments = segment_test()
-    # polygons = layer_graph(segments)
-    # plot_polygon_points(polygons)
 
 
 if __name__ == '__main__':
     import cProfile
-    # cProfile.runctx('main()', globals(), locals(), filename=None)
-    main()
+    cProfile.runctx('main()', globals(), locals(), filename=None)
+    # main()
     # plt.arrow(0,0,2,2, head_width=0.05, length_includes_head=True)
     # plt.show()

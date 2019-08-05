Adds fix for off-by-one pixel error in PIL

slicer/image_writer.py
@@ -5,27 +5,10 @@ Author: Drew Marschner
 
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+import os
 
-
-# FONT_BOLD = ImageFont.truetype(os.path.join(os.path.dirname(tkioutils.__file__), tkioutils.FONT_BOLD), 100)
-
-
-def transform_polygon(polygon, transform):
-    """
-    Transform a set of polygons according to a 3x3 transform
-    :param image:
-    :param transform:
-    :return:
-    """
-    polygon = np.array(polygon)
-
-    # Set to homogeneous coordinates
-    polygon = np.append(polygon, np.ones([len(polygon),1]), axis=1)
-
-    polygon = np.dot(polygon, transform)
-
-    return polygon[:, 0:2]
-
+# TODO: file location is a magic value, change how this is done?
+FONT_BOLD = ImageFont.truetype('./data/DINBold.ttf', size=100)
 
 def polygon_orientation(polygon):
     """
@@ -104,8 +87,8 @@ def polygon_size(polygon):
     """
     np_polygon = np.array(polygon)
 
-    bbox_x = np.max(np_polygon[:,0]) - np.min(np_polygon[:, 0])
-    bbox_y = np.max(np_polygon[:,1]) - np.min(np_polygon[:, 1])
+    bbox_x = np.max(np_polygon[:, 0]) - np.min(np_polygon[:, 0])
+    bbox_y = np.max(np_polygon[:, 1]) - np.min(np_polygon[:, 1])
 
     return bbox_x * bbox_y
 
@@ -131,19 +114,18 @@ def arrange_polygons(polygons):
     return arranged_polygons, is_hole
 
 
-def rasterize(polygons, output_file, layer, height, width, transform=None):
+def rasterize(polygons, layer, settings):
     """
 
     :param polygons:
-    :param output_file:
     :param layer:
-    :param height:
-    :param width:
-    :param transform:
+    :param settings:
     :return:
     """
+    filename = settings["slice_file_base_name"] + "_{}.bmp".format(layer)
+    output_file = os.path.join(settings["output_directory"], filename)
     arranged_polygons, is_hole = arrange_polygons(polygons)
-    im = Image.new("1", (width, height), 1)
+    im = Image.new("1", (settings["x_px"], settings["y_px"]), 1)
     draw = ImageDraw.Draw(im)
     for idx, polygon in enumerate(arranged_polygons):
         if len(polygon) < 2:
@@ -153,12 +135,11 @@ def rasterize(polygons, output_file, layer, height, width, transform=None):
         else:
             color = 0
 
-        if transform:
-            polygon = transform_polygon(polygon, transform)
+        # Round the polygon points before drawing the polygon. The PIL draw function uses a floor() instead of a
+        # round(), which can result in slightly incorrect bitmaps
+        polygon = np.round(polygon)
 
-        # Scale the polygon to get to the correct DPI
-        polygon = np.array(polygon)
-        polygon = tuple(map(tuple, polygon * 24.606))
+        polygon = tuple(map(tuple, polygon))
         draw.polygon(polygon, fill=color)
 
     # Flip and rotate the image...there has to be a better way to do this...
@@ -168,18 +149,7 @@ def rasterize(polygons, output_file, layer, height, width, transform=None):
 
     # Add text of layer number
     draw = ImageDraw.Draw(im)
-    # draw.text((10, 10), str(layer), fill=0, font=FONT_BOLD)
+    # TODO: make the location of the text a part of the settings
+    for location in settings["page_number_locations"]:
+        draw.text((location[0]*settings["dpi"][0], location[1]*settings["dpi"][1]), str(layer), fill=0, font=FONT_BOLD)
     im.save(output_file)
-
-
-class Polygon(object):
-    def __init__(self, polygons, layer):
-        self.polygons = polygons
-        self.layer = str(layer)
-        self.height = 4800
-        self.width = 7200
-        self.output_file = "{}.png".format(self.layer)
-
-
-if __name__ == '__main__':
-    pass

slicer/optimized_mesh.py
@@ -1,7 +1,9 @@
 import stl
 import numpy as np
 
-DECIMALS = 3
+
+MM_TO_IN = 25.4
+
 
 class MeshFace(object):
     def __init__(self, idx, vertex_indices):
@@ -12,6 +14,7 @@ class MeshFace(object):
         # via vertex index 0 and 1, etc.
         self.connected_face_index = []
         self.has_disconnected_faces = False
+
 
 class Vertex(object):
     def __init__(self, idx, p):
@@ -30,15 +33,46 @@ class OptimizedMesh(object):
     key (hash) : [array of indices representing vertices]
 
     """
-    def __init__(self, file):
+    def __init__(self, file, settings):
+        # Load mesh and transform it according to the desired settings
+        self.settings = settings
         self.mesh = stl.Mesh.from_file(file)
-        self.mesh_faces = np.round(self.mesh.vectors, decimals=DECIMALS)
+        self.transform_mesh()
+
+        self.mesh_faces = np.round(self.mesh.vectors, decimals=settings["decimal_precision"])
         self.vertex_hash_map = {}
         self.vertices = []
         self.faces = []
         self.add_faces()
 
+    def transform_mesh(self):
+        """
+        Modifies the mesh representation to be stretched according to the
+        DPI settings, the border settings, and converts from inch to metric if need be
+        :return:
+        """
+        translate = np.array(self.settings["mesh_translation_in"])
+        if self.settings["stl_units"] is "MM":
+            dpi_conversion_factor = 1/MM_TO_IN
+            translate = translate*MM_TO_IN
+        else:
+            dpi_conversion_factor = 1
+
+        # Translate the mesh
+        self.mesh.translate(translate)
+
+        # Scale the mesh. Mesh is scaled according to the dpi and the off-by-one pixel error found in PIL
+        x_scale = self.settings["dpi"][0]*dpi_conversion_factor*((self.settings["dpi"][0]-1)/self.settings["dpi"][0])
+        y_scale = self.settings["dpi"][1]*dpi_conversion_factor*((self.settings["dpi"][1]-1)/self.settings["dpi"][1])
+        z_scale = 1
+        scale = np.array([x_scale, y_scale, z_scale])
+        self.mesh.vectors *= scale
+
     def add_faces(self):
+        """
+        TODO: create documentation
+        :return:
+        """
         for triangle in self.mesh_faces:
             v0 = triangle[0]
             v1 = triangle[1]
@@ -105,7 +139,6 @@ class OptimizedMesh(object):
                 candidate_faces.append(connected_face)
 
         if not candidate_faces:
-            # print("Unconnected faces, bad mesh!")
             self.has_disconnected_faces = True
             return -1
 
@@ -113,11 +146,10 @@ class OptimizedMesh(object):
             return candidate_faces[0]
 
         if len(candidate_faces) % 2 == 0:
-            # print("Edge with uneven number of faces connecting it")
             self.has_disconnected_faces = True
 
         ##############################
-        # MATH STUFF - HERE BE DRAGONS
+        # MATH - HERE BE DRAGONS
         # - https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors
         ##############################
 
@@ -161,17 +193,13 @@ class OptimizedMesh(object):
                 best_idx = candidate_face
 
         if best_idx == -1:
-            # print("Mesh has disconected faces")
             self.has_disconnected_faces = True
-
-
         return best_idx
 
     def complete(self):
         self.vertex_hash_map = {}
         for i, face in enumerate(self.faces):
-            # print(i)
-            face.connected_face_index.append(self.get_face_idx_with_points(face.vertex_indices[0], face.vertex_indices[1], i, face.vertex_indices[2]))
+            face.connected_face_index.append(self.get_face_idx_with_points(face.vertex_indices[0],face.vertex_indices[1], i, face.vertex_indices[2]))
             face.connected_face_index.append(self.get_face_idx_with_points(face.vertex_indices[1], face.vertex_indices[2], i, face.vertex_indices[0]))
             face.connected_face_index.append(self.get_face_idx_with_points(face.vertex_indices[2], face.vertex_indices[0], i, face.vertex_indices[1]))
 